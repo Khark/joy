ab9a5883a95a546eda9c5102e88652d67f491a4c
Khark <kunoo913@naver.com>
2022-04-07 19:52:37 +0900

    joy renew

---
+++ joy/src/main/java/com/joy/demo/JoyApplication.java
@@ -0,0 +1,13 @@
+package com.joy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoyApplication.class, args);
+	}
+
+}
