fa4745115dcb5a5bef14fe8efe40f527bcb28504
Khark <kunoo913@naver.com>
2022-05-07 14:53:10 +0900

    ...

--- joy/src/main/java/com/joy/demo/entity/maria/boardEntity.java
+++ joy/src/main/java/com/joy/demo/entity/maria/boardEntity.java
@@ -12,7 +12,6 @@ import javax.persistence.Table;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicUpdate;
-import org.junit.runner.RunWith;
 
 import lombok.AccessLevel;
 import lombok.Builder;
