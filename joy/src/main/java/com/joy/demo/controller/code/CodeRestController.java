9488d517791345455215a35753f667f0019ad024
Khark <kunoo913@naver.com>
2022-07-11 22:07:49 +0900

    y ajax 405?

--- joy/src/main/java/com/joy/demo/controller/code/CodeRestController.java
+++ joy/src/main/java/com/joy/demo/controller/code/CodeRestController.java
@@ -2,21 +2,31 @@ package com.joy.demo.controller.code;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.joy.demo.dto.maria.code.codeReqDto;
 
 @RestController
 @RequestMapping("code/rest/*")
 public class CodeRestController {
 
+	
+	//public String codeForm(@RequestBody codeReqDto codeto , ModelMap model, Authentication authenticatio)  {
 	@PostMapping("codeForm")
-	public String codeForm( codeReqDto codeto , ModelMap model, Authentication authenticatio)  {
-		String result = "";
-	System.out.println("####codeReestofrm");
+	public String codeForm( ModelMap model)  {
+		String result = "ProcessSuccess";
+		System.out.println("####codeReestofrm");
 		return result;
 	} 
+
 	
+
+	@GetMapping("codeForm")
+	//public String codeForm(@RequestBody codeReqDto codeto , ModelMap model, Authentication authenticatio)  {
+	public String codeFormget( ModelMap model)  {
+		String result = "ProcessSuccess";
+		System.out.println("####codeReestofrm");
+		return result;
+	} 
 }
